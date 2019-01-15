264827876bb3cf925c64bfb250d6bbe52d8161c8
Carlos Mañas <carlos@spacenomads.com>
2019-01-15 10:29:51 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const fr = new FileReader();
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.fileInput = React.createRef();
+    this.state = {
+      fileName: 'Nombre de archivo por defecto',
+      fileUrl: 'https://placehold.it/200x200'
+    }
+    this.fakeClick = this.fakeClick.bind(this);
+    this.handleFileChange = this.handleFileChange.bind(this);
+    this.writeImage = this.writeImage.bind(this);
+  }
+
+  fakeClick(){
+    console.log('Yay');
+    this.fileInput.current.click();
+  }
+
+  writeImage() {
+    const url = fr.result;
+    this.setState({
+      fileUrl: url
+    });
+  }
+
+  handleFileChange(e) {
+    const myFile = e.currentTarget.files[0];
+    const name = myFile.name;
+    
+    
+    fr.addEventListener('load', this.writeImage);
+    fr.readAsDataURL(myFile);
+
+    this.setState({
+      fileName: name
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <input type="file" ref={this.fileInput} onChange={this.handleFileChange} />
+
+        <button onClick={this.fakeClick}>Click</button>
+
+        <h2>{this.state.fileName}</h2>
+
+        <img className="img" src={this.state.fileUrl} alt={this.state.fileName} />
+
+        <div className="bg" style={{backgroundImage: `url(${this.state.fileUrl})` }}></div>
+
       </div>
     );
   }
